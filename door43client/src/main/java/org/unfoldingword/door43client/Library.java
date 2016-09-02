4a97adbeaad616d9c611ea0059c270cd6e178d93
Joel Lonbeck <joel@neutrinographics.com>
2016-09-02 13:08:11 -0700

    added utility to insert or update a row

--- door43client/src/main/java/org/unfoldingword/door43client/Library.java
+++ door43client/src/main/java/org/unfoldingword/door43client/Library.java
@@ -3,6 +3,9 @@ package org.unfoldingword.door43client;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.text.TextUtils;
+
+import java.util.Map;
 
 /**
  * Manages the indexed library content.
@@ -28,6 +31,49 @@ public class Library {
      */
     private void validateNotEmpty(String value) throws Exception {
         if(value == null || value.isEmpty()) throw new Exception("Invalid parameter value");
+    }
+
+    /**
+     * A utility to perform insert+update operations.
+     * Insert failures are ignored.
+     * Update failures are thrown.
+     *
+     * @param table
+     * @param values
+     * @param uniqueColumns an array of unique columns on this table. This should be a subset of the values.
+     * @return the id of the inserted/updated row
+     */
+    private long insertOrUpdate(String table, ContentValues values, String[] uniqueColumns) throws Exception {
+        // insert
+        long id = db.insertWithOnConflict(table, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+        if(id == -1) {
+            // prepare unique columns
+            String uniqueWhere = TextUtils.join("=? and ", uniqueColumns) + "=?";
+            String[] uniqueValues = new String[uniqueColumns.length];
+            for(int i = 0; i < uniqueColumns.length; i ++) {
+                uniqueValues[i] = String.valueOf(values.get(uniqueColumns[i]));
+            }
+
+            // clean values
+            for(String key:uniqueColumns) {
+                values.remove(key);
+            }
+
+            // update
+            int numRows = db.updateWithOnConflict(table, values, uniqueWhere, uniqueValues, SQLiteDatabase.CONFLICT_ROLLBACK);
+            if(numRows == 0) {
+                throw new Exception("Failed to update the row in " + table);
+            } else {
+                // retrieve updated row id
+                Cursor cursor = db.rawQuery("select id from " + table + " where " + uniqueWhere, uniqueValues);
+                if(cursor.moveToFirst()) {
+                    id = cursor.getLong(0);
+                } else {
+                    throw new Exception("Failed to find the row in " + table);
+                }
+            }
+        }
+        return id;
     }
 
     /**
