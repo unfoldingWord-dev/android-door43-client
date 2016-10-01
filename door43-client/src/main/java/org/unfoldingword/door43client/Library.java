707140c34ff3a9f7cd166b59c325a25c0d49f336
Joel Lonbeck <joel@neutrinographics.com>
2016-09-30 23:50:47 -0700

    moved whereclause class into it's own file

--- door43-client/src/main/java/org/unfoldingword/door43client/Library.java
+++ door43-client/src/main/java/org/unfoldingword/door43client/Library.java
@@ -1224,65 +1224,6 @@ class Library implements Index {
     }
 
     /**
-     * This is a utility class for preparing a where clause
-     */
-    private static class WhereClause {
-        public final String statement;
-        public final String[] arguments;
-
-        private WhereClause(String statement, String[] values) {
-            this.statement = statement;
-            this.arguments = values;
-        }
-
-        /**
-         * Performs a bunch of magical operations to convert a set of values and the specified unique columns
-         * into a valid where clause with supporting values.
-         *
-         *
-         *
-         * @param values
-         * @param uniqueColumns
-         * @return
-         */
-        public static WhereClause prepare(ContentValues values, String[] uniqueColumns) {
-            List<String> stringColumns = new ArrayList<>();
-            List<String> numberColumns = new ArrayList<>();
-
-            // split columns into sets by type
-            for(String key:uniqueColumns) {
-                if(values.get(key) instanceof String) {
-                    stringColumns.add(key);
-                } else {
-                    numberColumns.add(key);
-                }
-            }
-
-            // build the statement
-            String whereStmt = "";
-            if(stringColumns.size() > 0) {
-                whereStmt = TextUtils.join("=? and ", stringColumns) + "=?";
-            }
-            if(numberColumns.size() > 0) {
-                if (!whereStmt.isEmpty()) whereStmt += " and ";
-                List<String> expressions = new ArrayList<>();
-                for(String key:numberColumns) {
-                    expressions.add(key + "=" + values.get(key));
-                }
-                whereStmt += TextUtils.join(" and ", expressions);
-            }
-
-            // build the values
-            String[] uniqueValues = new String[stringColumns.size()];
-            for(int i = 0; i < stringColumns.size(); i ++) {
-                uniqueValues[i] = String.valueOf(values.get(stringColumns.get(i)));
-            }
-
-            return new WhereClause(whereStmt, uniqueValues);
-        }
-    }
-
-    /**
      * A helper class to make reading from a cursor easier.
      */
     private static class CursorReader {
